3c74e965f84f878248efc3618d988f5941526439
Deus-Ex-Umbra <gab.aparicio.ll@gmail.com>
2025-05-13 01:25:47 -0400

    feature-nuevastablas

---
+++ modules/Ofertas/Models/TipoOferta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Ofertas\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class TipoOferta extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tipo_ofertas';
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'cantidad_minima_productos',
+        'cantidad_maxima_productos'
+    ];
+
+}
